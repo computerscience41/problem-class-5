d011d3600d3bddd0c0965e121e62fe53af98a0ef
computerscience41 <rakibhasan934541@gmail.com>
2024-11-14 18:16:58 +0600

    New file

---
+++ new-file.py
@@ -0,0 +1,6 @@
+# Problem - 6 Creation and  writing to a new file
+
+with open('notes.txt', 'w') as file:
+    file.write("Python is great for data analysis.\n")
+    file.write("Exception handling makes code robust.\n")
+    file.write("File handling is crucial for data storage.\n")
